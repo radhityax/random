c86ea0ae0e1e6a0f9d36ae6483bd741ec3a5d375
radhitya <alif@radhitya.org>
2025-09-04 10:45:41 +0700

    hmm

--- pid_simulation.c
+++ pid_simulation.c
@@ -20,6 +20,7 @@ pi_ctrl(float setpoint, float actual, float kp,
 	*integral += error;
 	return kp * error + ki * *integral;
 }
+
 float
 pid_ctrl(float setpoint, float actual, float kp,
 	float ki, float kd, float *integral,
@@ -31,9 +32,47 @@ pid_ctrl(float setpoint, float actual, float kp,
 	*prev_error = error;
 	return kp * error + ki * *integral + kd * derivative;
 }
-int main(void) {
-	float setpoint, actual, kp;
-	printf("setpoint, actual, kp, ki: \n");
-	scanf("%f %f %f", &setpoint, &actual, &kp);
-	printf("result: %f\n", p_ctrl(setpoint, actual, kp));
+
+int
+main(void) {
+	float setpoint, actual, kp, ki, kd, *integral, *prev_error;
+	float result = 0;
+	int counter = 0;
+	char *option = malloc(sizeof(char) * 8);
+	printf("pid simulation\np - p controller \npi - pi controller\n"
+			"pid - pid controller\n");
+	while(1) {
+		printf("option: ");
+		fgets(option, sizeof(option), stdin);
+		option[strcspn(option, "\n")] = 0;
+		if (strcmp(option, "p") == 0)  {
+			printf("setpoint: ");
+			scanf("%f", &setpoint);
+			printf("actual: ");
+			scanf("%f", &actual);
+			printf("kp: ");
+			scanf("%f", &kp);
+			result = p_ctrl(setpoint, actual, kp);
+			printf("hasilnya: %.2f\n", result);
+			while(1) {
+				counter++;
+				actual += result;
+				printf("%d %f %f %f\n", counter, setpoint, actual, kp);
+				result = p_ctrl(setpoint, actual, kp);
+				if(fabs(setpoint - actual) < 0.01) {
+					printf("good job :)\n");
+					printf("%d %f %f %f\n", counter, setpoint, actual, result);
+					break;
+				}
+			}
+		}
+		else if(strcmp(option, "pi") == 0) {
+		}
+		else if(strcmp(option, "pid") == 0) {
+		}
+		else {
+			printf("goodbye :(\n");
+			break;
+		}
+	}
 }
